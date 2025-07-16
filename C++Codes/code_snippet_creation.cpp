0d440d58231c3d805d770d46edeb26d91c3ef2f1
“codealpha_tasks <ijazhamza2005@gmail.com>
2025-07-16 15:48:53 +0500

    C++

---
+++ C++Codes/code_snippet_creation.cpp
@@ -0,0 +1,11 @@
+// Typed c++ and code snippet
+//  This code snippet is a simple C++ program that includes the iostream library
+//  and uses the standard namespace. It contains a main function that returns 0.
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    return 0;
+}
