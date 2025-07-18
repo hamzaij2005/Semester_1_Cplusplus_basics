039028aea62421e98e8a6325170e90ace938f25a
“codealpha_tasks <ijazhamza2005@gmail.com>
2025-07-18 19:13:22 +0500

    Commented

---
+++ C++Codes/multiplication.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    float a, b;
+    cout << "Enter 1 integers: ";
+    cin >> a ;
+    cout << "Enter 2 integers: ";
+    cin >> b;
+    float result = a * b;
+    cout << "The result of multiplication is: " << result << endl;     
+    // End of the program
+    return 0;
+}
